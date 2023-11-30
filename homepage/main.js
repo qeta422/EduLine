0f06494e16e1123baddae301319f8f4afa0c6c8d
qeta422 <qetafetviashvili123@gmail.com>
2023-11-30 21:20:40 +0400

    cards scroll function added homepage

--- homepage/main.js
+++ homepage/main.js
@@ -1,27 +1,23 @@
-// let postCards = document.getElementsByClassName("post-cards");
-
-// window.onscroll = () => {
-//     for (let i = 0; i < postCards.length; i++) {
-//         let sec = postCards[i];
-//         let top = window.scrollY;
-//         let offset = sec.offsetTop;
-//         let height = sec.offsetHeight;
-
-//         if (top >= offset && top < offset + height) {
-//             sec.classList.remove('show-animate');
-//         } else {
-//             sec.classList.add('show-animate');
-//         }
-//     }
-// }
-
+let postCards = document.getElementsByClassName("post-cards");
 let faqAsides = document.getElementsByClassName("faq-asides");
 
 window.onscroll = () => {
+    for (let i = 0; i < postCards.length; i++) {
+        let sec = postCards[i];
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+
+        if (top >= offset && top < offset + height) {
+            sec.classList.remove('show-animate');
+        } else {
+            sec.classList.add('show-animate');
+        }
+    }
     for (let i = 0; i < faqAsides.length; i++) {
         let sec = faqAsides[i];
         let top = window.scrollY;
-        let offset = sec.offsetTop -250;
+        let offset = sec.offsetTop -260;
         let height = sec.offsetHeight;
 
         if (top >= offset && top < offset + height) {
