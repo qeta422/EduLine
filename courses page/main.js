8989daaa11b4f8c56fe735e47845b2da4e9751ff
qeta422 <qetafetviashvili123@gmail.com>
2023-12-01 18:04:09 +0400

    right filter functional js

--- courses page/main.js
+++ courses page/main.js
@@ -1,10 +1,14 @@
-let postCards = document.getElementsByClassName("post-cards");
+let postCards = document.querySelectorAll(".post-cards");
+let postCard = document.querySelectorAll(".post-card");
+let filter = document.querySelector('.right-filter');
+let buttons = filter.querySelectorAll('button');
+let listItems = Array.from(postCard);
 
 window.onscroll = () => {
     for (let i = 0; i < postCards.length; i++) {
         let sec = postCards[i];
         let top = window.scrollY;
-        let offset = sec.offsetTop -260;    
+        let offset = sec.offsetTop - 260;
         let height = sec.offsetHeight;
 
         if (top >= offset && top < offset + height) {
@@ -14,3 +18,24 @@ window.onscroll = () => {
         }
     }
 }
+function filterData(searchItem) {
+    buttons.forEach((button) => {
+        button.classList.remove('active');
+    });
+
+    listItems.forEach((item) => {
+        if (item.innerText.toLowerCase().includes(searchItem.toLowerCase())) {
+            item.classList.remove('hidden');
+        } else {
+            item.classList.add('hidden');
+        }
+    });
+}
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        const clickedButton = event.target;
+        filterData(clickedButton.innerText);
+        clickedButton.classList.add('active');
+    });
+});
